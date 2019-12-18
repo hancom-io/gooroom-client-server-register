fb7181c306213fd70172366b281b18551a573470
junsungc <junsungc@gooroom.kr>
2019-12-18 11:59:53 +0900

    Change common-name(cn) value. [mac -> mac+hash(hardware SN)]
    Change-Id: I768b46a080abad7354952b02b878aa1834c7a12f

--- usr/lib/gooroom/gooroomClientServerRegister/registering.py
+++ usr/lib/gooroom/gooroomClientServerRegister/registering.py
@@ -6,6 +6,8 @@ import pprint
 import threading
 import time
 import copy
+import hashlib
+import codecs
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -91,24 +93,22 @@ class Registering():
 
         return result_text
 
-    def make_cn(self):
+    def make_hash_cn(self):
+        cmd = subprocess.run(['/usr/sbin/dmidecode', '-s', 'system-serial-number'], stdout=subprocess.PIPE, universal_newlines=True)
+        result = cmd.stdout.rstrip() + '/'
+        cmd = subprocess.run(['/usr/sbin/dmidecode', '-s', 'system-uuid'], stdout=subprocess.PIPE, universal_newlines=True)
+        result += cmd.stdout.rstrip() + '/'
+        cmd = subprocess.run(['/usr/sbin/dmidecode', '-s', 'baseboard-serial-number'], stdout=subprocess.PIPE, universal_newlines=True)
+        result += cmd.stdout.rstrip()
+        hash_result = hashlib.md5(result.encode()).hexdigest()
+        base64_result = codecs.encode(codecs.decode(hash_result, 'hex'), 'base64').decode().rstrip()
+        return base64_result
+
+    def make_mac(self):
         """
         make cn with sn + mac
         """
 
-        CN_PATH = '/etc/gooroom/gooroom-client-server-register/gcsr.conf'
-        if os.path.exists(CN_PATH):
-            try:
-                import configparser
-                parser = configparser.RawConfigParser()
-                parser.optionxform = str
-                parser.read(CN_PATH)
-                cn = parser.get('certificate', 'client_name').strip().strip('\n')
-                print('gcsr.conf={}'.format(cn))
-                return cn
-            except:
-                pass
-            
         ENP_PATH = '/sys/class/net/enp0s3/address'
         if os.path.exists(ENP_PATH):
             with open(ENP_PATH) as f:
@@ -127,6 +127,24 @@ class Registering():
                     print('iface={}'.format(cn))
                     return cn
             return 'CN-NOT-FOUND-ERROR'
+
+    def make_cn(self):
+
+        CN_PATH = '/etc/gooroom/gooroom-client-server-register/gcsr.conf'
+        if os.path.exists(CN_PATH):
+            try:
+                import configparser
+                parser = configparser.RawConfigParser()
+                parser.optionxform = str
+                parser.read(CN_PATH)
+                cn = parser.get('certificate', 'client_name').strip().strip('\n')
+                print('gcsr.conf={}'.format(cn))
+                return cn
+            except:
+                pass
+
+        cn = self.make_mac()
+        return cn + self.make_hash_cn()
 
     def make_ipname(self):
         """
